4660976be73e7d8fde6c462e8665e67641f70b95
Breanna <b.gonzales21@yahoo.com>
2023-09-26 17:46:44 -0700

    added files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import Authenticate from "./authenticate";
+import SignUpForm from "./signupform";
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
